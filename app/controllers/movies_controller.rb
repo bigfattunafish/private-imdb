ae6c567588c4fc1cf6056f978429c9d7cd8dfcb8
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-10 19:39:05 -0400

    Revised movies_controller to check for a duplicate movie; IF so, redirect to saved_already.html.erb page which was newly created

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,21 +11,24 @@ class MoviesController < ApplicationController
   def save
     movie_to_pulldown = Imdb::Movie.new(params[:movie_id])
 
-    # if Movie.all.include?(movie_to_pulldown)
-    #   redirect_to('/alreadysaved')
-    # else
-    movie_to_save = Movie.new
+    if Movie.all.map(&:title).include?(movie_to_pulldown.title)
+      redirect_to(saved_already_path)
+    else
+      movie_to_save = Movie.new
 
-    movie_to_save.title = movie_to_pulldown.title
-    movie_to_save.year = movie_to_pulldown.year
-    movie_to_save.plot = movie_to_pulldown.plot
-    movie_to_save.mpaa_rating = movie_to_pulldown.mpaa_rating
-    movie_to_save.tomatoes = 50
-    movie_to_save.save
+      movie_to_save.title = movie_to_pulldown.title
+      movie_to_save.year = movie_to_pulldown.year
+      movie_to_save.plot = movie_to_pulldown.plot
+      movie_to_save.mpaa_rating = movie_to_pulldown.mpaa_rating
+      movie_to_save.tomatoes = 50
+      movie_to_save.save
 
-    redirect_to('/movies')
+      redirect_to('/movies')
+    end
     # movie_to_pulldown.cast_members
+  end
 
+  def saved_already
   end
 
 end
