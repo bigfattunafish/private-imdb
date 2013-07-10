6b7d0b6c836cdf0e22298de8ed7ef70257e6ac91
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-10 16:34:02 -0400

    Created actors_controller and movies_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 RailsImdb::Application.routes.draw do
   root :to => 'home#index'
   resources :movies
+
+#     root        /                          home#index
+#     movies GET    /movies(.:format)          movies#index
+#            POST   /movies(.:format)          movies#create
+#  new_movie GET    /movies/new(.:format)      movies#new
+# edit_movie GET    /movies/:id/edit(.:format) movies#edit
+#      movie GET    /movies/:id(.:format)      movies#show
+#            PUT    /movies/:id(.:format)      movies#update
+#            DELETE /movies/:id(.:format)      movies#destroy
+
+
 end
