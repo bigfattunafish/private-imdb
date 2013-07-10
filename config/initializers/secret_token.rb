43e1b94d65351ed8f0b81b10f8acb7357833f518
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-10 15:31:34 -0400

    First commit with new rails environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsImdb::Application.config.secret_token = '5e60fe5e34cbba26c3808ebda1c66cd5ce4db4926d8cef8fefb439a3fe127e627fc0d233f1410f7076f5bcbc8ef4fe50713fe55d1942d5856f3913cfd80ae0b2'
